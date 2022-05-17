edc7615460a66fcfa91e5c88eddaada296c2f2f4
Takeo Yukihiro <takeo.takeo331@gmail.com>
2022-05-17 22:56:04 +0900

    Add localize

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:question_ozisan/page/ozisan_page/page.dart';
 
@@ -20,6 +21,14 @@ class MyApp extends HookConsumerWidget {
         primarySwatch: Colors.green,
         useMaterial3: true,
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("ja", "JP"),
+      ],
       home: const OzisanPage(),
     );
   }
